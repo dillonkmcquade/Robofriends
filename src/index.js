98302ff82b046e0724fe6a20e84b5690c39ad53d
Dillon McQuade <dillonkmcquade@gmail.com>
2019-12-03 17:38:04 -0500

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import PrimarySearchAppBar from './appBar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<PrimarySearchAppBar />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
