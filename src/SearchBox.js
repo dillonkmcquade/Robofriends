987a1357ced8ac13341ab9fc189d7f041adc0ef5
Dillon McQuade <dillonkmcquade@gmail.com>
2019-12-05 20:10:37 -0500

    added functionality to searchbox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchField, searchChange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--white shadow-5 bg-lightest-blue br4"
         type="search"
         placeholder="search robots.."
+        onChange= {searchChange}
       />
     </div>
   );
